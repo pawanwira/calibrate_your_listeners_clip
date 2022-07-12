1fe311bf6c1f3a03de57c339658dd2e31054ec96
Pawan <pawanwira@gmail.com>
2022-07-12 16:44:26 -0700

    Minor updates for CLIP listener system

--- calibrate_your_listeners/src/objectives/clip_listener_scores.py
+++ calibrate_your_listeners/src/objectives/clip_listener_scores.py
@@ -14,44 +14,46 @@ class CLIPListenerScores(object):
 
         self.listener_scores = self._calculate_listener_scores()
 
+    def _preprocess(self, image):
+        return self.preprocess(Image.fromarray(np.uint8(image.cpu())).convert('RGB'))
+
+    def _get_utterance_tokens(self, i):
+        if self.vocab_type == "gpt2":
+            lang = self.df['speaker_utterance'][i][:-13]
+        elif self.vocab_type == "shapeworld":
+            lang = self.df['speaker_utterance'][i].replace("<sos>", "").replace("<eos>", "").replace("<UNK>", "").replace("<PAD>", "")
+            lang = " ".join(lang.split())
+
+        if len(lang):
+            if lang[0] in ['a', 'e', 'i', 'o', 'u']:
+                utterance = "This is an " + lang 
+            else:
+                utterance = "This is a " + lang
+        else:
+            utterance = " "
+
+        utterance_tokens = clip.tokenize(utterance).cuda()
+        return utterance_tokens
+
     def _calculate_listener_scores(self):
         # import pdb; pdb.set_trace()
         lis_scores = []
         for i in range(len(self.imgs)):
             states = self.imgs[i]
-            images = []
-            for j in range(3):
+            images = torch.tensor(np.stack([self._preprocess(state) for state in states])).cuda()
+
+            """for j in range(3):
                 image = Image.fromarray(np.uint8(states[j].cpu())).convert('RGB')
                 images.append(self.preprocess(image))
-            image_pre = torch.tensor(np.stack(images)).cuda() 
+            images_pre = torch.tensor(np.stack(images)).cuda()""" 
 
-            # import pdb; pdb.set_trace()
-            if self.vocab_type == "gpt2":
-                lang_pre = self.df['speaker_utterance'][i][:-13]
-            elif self.vocab_type == "shapeworld":
-                lang_pre = self.df['speaker_utterance'][i].replace("<sos>", "").replace("<eos>", "").replace("<UNK>", "").replace("<PAD>", "")
-                lang_pre = " ".join(lang_pre.split())
-            
-            print(len(lang_pre))
-            print(lang_pre)
-
-            if len(lang_pre):
-                if lang_pre[0] in ['a', 'e', 'i', 'o', 'u']:
-                    utterance = "This is an " + lang_pre 
-                else:
-                    utterance = "This is a " + lang_pre
-            else:
-                utterance = " "
-
-            utterance_tokens = clip.tokenize(utterance).cuda()
+            utterance_tokens = self._get_utterance_tokens(i)
  
             with torch.no_grad():
-                image_features = self.listener.encode_image(image_pre).float()
+                image_features = self.listener.encode_image(images).float()
                 utterance_features = self.listener.encode_text(utterance_tokens).float()
             
-            # image_features = self.listener.encode_image(image_pre).float()
-            image_features = self.listener.encode_image(image_pre).float()
-            # utterance_features = self.listener.encode_text(utterance_tokens).float()
+            image_features = self.listener.encode_image(images).float()
             utterance_features = self.listener.encode_text(utterance_tokens).float()
             # image_features /= image_features.clone().norm(dim=-1, keepdim=True)
             image_features = image_features.clone() / image_features.clone().norm(dim=-1, keepdim=True)
@@ -61,7 +63,7 @@ class CLIPListenerScores(object):
             lis_scores.append(image_probs[0])
 
         # import pdb; pdb.set_trace()
-        lis_scores_final = torch.stack(lis_scores)  
+        lis_scores_final = torch.stack(lis_scores)
         return lis_scores_final
 
     def get_average_l0_score(self):
